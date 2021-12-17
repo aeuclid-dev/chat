c1056a8aba16f5277433bd4a901c7b53231c7eba
novemberizing <novemberizing@gmail.com>
2021-12-18 08:48:09 +0900

    update!

--- README.md
+++
@@ -1,2 +0,0 @@
-# chat
-novemberizing's webrtc chatting client
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './middle.css';
-import './cover.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
