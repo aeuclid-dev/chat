f848e7bcc84ffae4a4bdae1f572cc3149a1ec840
novemberizing <novemberizing@gmail.com>
2021-12-17 11:27:31 +0900

    daily

--- src/app.js
+++ src/app.js
@@ -5,56 +5,27 @@ export default class App extends React.Component {
         super(props);
     }
     render() {
-        return [
-            <header className="mb-auto">
-                <div>
-                    <h3 className="float-md-start mb-0"><strong>X WEB VIDEO CHAT</strong></h3>
-                    <nav className="nav nav-masthead justify-content-center float-md-end">
-                        <a className="nav-link active" aria-current="page" href="#">HOME</a>
-                        <a className="nav-link" href="#">FEATURE</a>
-                        <a className="nav-link" href="#">CONTACT</a>
-                    </nav>
-                </div>
-            </header>,
-            <main className="px-3">
-                <form>
-                    <div className="row">
-                        <div className="col-3"></div>
-                        <div className="col-6 text-start">
-                            <label for="email" className="form-label"><strong>Email address</strong></label>
-                            <input type="email" className="form-control" id="email" aria-describedby="help" />
-                            <div id="help" className="form-text">We'll never share your email with anyone else.</div>
-                        </div>
-                        <div className="col-3"></div>
-                    </div>
-                    <div className="row">
-                        <div className="col-3"></div>
-                        <div className="col-6 text-start">
-                            <div className="form-check form-check-inline">
-                                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked />
-                                <label className="form-check-label" for="flexRadioDefault1">Client</label>
-                            </div>
-                            <div className="form-check form-check-inline">
-                                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-                                <label className="form-check-label" for="flexRadioDefault2">Server</label>
-                            </div>
-                        </div>
-                        <div className="col-3"></div>
-                    </div>
-                    <div className="row mt-4">
-                        <div className="col-3"></div>
-                        <div className="col-6 text-end">
-                            <button type="submit" className="btn btn-outline-light" onClick="">Confirm</button>
-                        </div>
-                        <div className="col-3"></div>
-                    </div>
-                </form>
-            </main>,
-            <footer className="mt-auto text-white-50">
-                <p>
-                    NOVEMBERIZING | ABOUT 
-                </p>
-            </footer>
-        ]
+        return <div className="list-group list-group-checkable">
+            <input className="list-group-item-check" type="radio" name="listGroupCheckableRadios" style={{display: 'none'}} id="listGroupCheckableRadios1" value="" defaultChecked />
+            <label className="list-group-item py-3" htmlFor="listGroupCheckableRadios1">
+                First radio
+                <span className="d-block small opacity-50">With support text underneath to add more detail</span>
+            </label>
+            <input className="list-group-item-check" type="radio" name="listGroupCheckableRadios" style={{display: 'none'}} id="listGroupCheckableRadios2" value="" />
+            <label className="list-group-item py-3" htmlFor="listGroupCheckableRadios2">
+                Second radio
+                <span className="d-block small opacity-50">Some other text goes here</span>
+            </label>
+            <input className="list-group-item-check" type="radio" name="listGroupCheckableRadios" style={{display: 'none'}} id="listGroupCheckableRadios3" value="" />
+            <label className="list-group-item py-3" htmlFor="listGroupCheckableRadios3">
+                Third radio
+                <span className="d-block small opacity-50">And we end with another snippet of text</span>
+            </label>
+            <input className="list-group-item-check" type="radio" name="listGroupCheckableRadios" style={{display: 'none'}} id="listGroupCheckableRadios4" value="" />
+            <label className="list-group-item py-3" htmlFor="listGroupCheckableRadios4">
+                Fourth disabled radio
+                <span className="d-block small opacity-50">This option is disabled</span>
+            </label>
+        </div>
     }
 };
