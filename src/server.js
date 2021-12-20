310f62c08102270dd4d438cdc316e7e2958f160f
novemberizing <novemberizing@gmail.com>
2021-12-20 15:31:22 +0900

    add v1

--- src/server.js
+++ src/server.js
@@ -14,6 +14,10 @@ app.use(
   })
 );
 
+app.get('/v1/user/register/', (req, res) => {
+  res.send('Hello World!');
+});
+
 // Serve the files on port 3000.
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!\n');
