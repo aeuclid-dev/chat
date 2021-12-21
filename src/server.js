8d79f2b443c41321e915f0ad0a3480596bd3e2c2
novemberizing <novemberizing@gmail.com>
2021-12-21 11:11:45 +0900

    implement register!

--- src/server.js
+++ src/server.js
@@ -6,6 +6,21 @@ const app = express();
 const cors = require('cors');
 const config = require('../config/webpack.dev.js');
 const compiler = webpack(config);
+const mysql = require('mysql2');
+
+const database = {
+  host: 'localhost',
+  port: '3306',
+  user: 'root',
+  password: 'novemberizing@21',
+  database: 'xchat',
+  connectionLimit: 32,
+  insecureAuth : true
+};
+
+const pool = mysql.createPool(database);
+
+console.log(pool);
 
 app.use(
   webpackDevMiddleware(compiler, {
@@ -25,15 +40,35 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions)); // 옵션을 추가한 CORS 미들웨어 추가
-
-// app.use(cors(corsOptions));
+app.use(express.json());
 
 app.post('/v1/user/register/', (req, res) => {
-  res.send('user register');
-});
-
-app.get('/v1/user/register/', (req, res) => {
-  res.send('user register');
+  if(req.body.email) {
+    pool.getConnection(function(error, connection) {
+      if(!error) {
+        const sql = `CALL REGISTER_USER(?,?,?)`;
+        // const sql = `INSERT INTO TB_USER (\`email\`, \`name\`, \`description\`, \`like\`, \`datetime\`) VALUES (?, ?, ?, 5, CURRENT_TIMESTAMP())`;
+        const params = [req.body.email, req.body.name, req.body.description];
+        connection.query(sql, params, function(error, result) {
+          if(!error) {
+            console.log('ok');
+            res.send({'message': 'ok'});
+          } else {
+            console.log(error);
+            res.status(500).send({'error': 'internal storage error'});
+          }
+        });
+      } else {
+        console.log(error);
+        res.status(500).send({'error': 'server busy'});
+      }
+      if(connection) {
+        connection.release();
+      }
+    });
+  } else {
+    res.status(500).send({'error': 'invalid param'});
+  }
 });
 
 app.get('/v1/room/create/', (req, res) => {
